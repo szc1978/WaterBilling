ec3568d2eda34f29e86cddc7925380e022517bbe
Jims Su <29258678@qq.com>
2017-03-22 22:31:15 +0800

    add oplog

--- src/main/java/org/water/billing/controller/SysController.java
+++ src/main/java/org/water/billing/controller/SysController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.water.billing.entity.LoginHistory;
 import org.water.billing.service.LoginHistoryService;
@@ -16,7 +17,7 @@ public class SysController {
 	LoginHistoryService loginHistoryService;
 	
 	@RequestMapping("/admin/loginhistory")
-	public String addUser(@RequestParam(value="page",required=false) String page,ModelMap map) {
+	public String loginHistory(@RequestParam(value="page",required=false) String page,ModelMap map) {
 		int index = (page == null) ? 0 : Integer.valueOf(page);
 		Page<LoginHistory> historys = loginHistoryService.findAll(index,10);
 		long total = loginHistoryService.count();
@@ -26,4 +27,9 @@ public class SysController {
 		return "/loginhistory";
 	}
 	
+	@RequestMapping(value="/admin/oplog",method=RequestMethod.GET)
+	public String opLog(@RequestParam(value="page",required=false) String page,ModelMap map) {
+		
+		return "/oplog";
+	}
 }
